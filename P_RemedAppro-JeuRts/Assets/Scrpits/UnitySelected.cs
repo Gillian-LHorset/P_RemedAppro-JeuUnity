13d2c44192eeb1d0b2888f6e633e7af4d9fa03f4
Gillian-LHorset <pq60soi@eduvaud.ch>
2026-02-06 10:46:58 +0100

    feat(programme): Ajout d'un personnage de test et d'un background

---
+++ P_RemedAppro-JeuRts/Assets/Scrpits/UnitySelected.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using System.Collections
+
+using System.Diagnostics;
+
+public class UnitySelected : MonoBehaviour {
+    void Update() {
+        if (Input.GetMouseButtonDown0) {
+            Debug.Log("clique gauche")
+        }
+    }
+}
