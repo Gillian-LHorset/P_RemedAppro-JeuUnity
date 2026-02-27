442e1eb63bc4c1f6d769aa449800a55d099c61e7
Gillian-LHorset <pq60soi@eduvaud.ch>
2026-02-27 09:59:58 +0100

    feat(system unite select): Ajout d'un système de selection et déplacement d'unité
    [300][DONE]

--- P_RemedAppro-JeuRts/Assets/Scrpits/SelectUniteScript.cs
+++ P_RemedAppro-JeuRts/Assets/Scrpits/SelectUniteScript.cs
@@ -4,17 +4,17 @@ using System.Diagnostics;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class SelectUniteScript : MonoBehaviour
-{
+public class SelectUniteScript : MonoBehaviour {
     private Camera mainCamera;
 
-    List<gameObject> selectedUnite;
-
+    List<GameObject> selectedUnite = new List<GameObject>();
+    public float moveSpeed = 5f;
     void Awake() {
         mainCamera = Camera.main;
     }
 
     void Update() {
+        // left click logic
         if (Mouse.current.leftButton.wasPressedThisFrame) {
             Vector2 mousePosition = Mouse.current.position.ReadValue();
             Vector2 worldPosition = mainCamera.ScreenToWorldPoint(mousePosition);
@@ -23,8 +23,34 @@ public class SelectUniteScript : MonoBehaviour
             if (hit.collider != null && hit.collider.CompareTag("Unite")) {
                 UnityEngine.Debug.Log("Unité cliquée : " + hit.collider.gameObject.name);
 
-                selectedUnite.Add(gameObject);
+                selectedUnite.Add(hit.collider.gameObject);
             }
+        }
+        foreach (GameObject unite in selectedUnite) {
+            unite.GetComponent<SpriteRenderer>().color = Color.blue;
+        }
+
+        // right click logic
+        if (Mouse.current.rightButton.wasPressedThisFrame && selectedUnite.Count > 0) {
+            Vector2 mousePosition = Mouse.current.position.ReadValue();
+            Vector2 worldPosition = mainCamera.ScreenToWorldPoint(mousePosition);
+
+
+            foreach (GameObject unite in selectedUnite) {
+                StartCoroutine(MoveUnit(unite, worldPosition));
+            }
+        }
+    }
+
+    private IEnumerator MoveUnit(GameObject unite, Vector2 targetPosition) {
+        Vector3 target = new Vector3(targetPosition.x, targetPosition.y, unite.transform.position.z); // this is a 3d vector for keep the z position of the unite
+        while (Vector3.Distance(unite.transform.position, target) > 0.1f) {
+            unite.transform.position = Vector3.MoveTowards(
+                unite.transform.position,
+                target,
+                moveSpeed * Time.deltaTime
+            );
+            yield return null;
         }
     }
 }
